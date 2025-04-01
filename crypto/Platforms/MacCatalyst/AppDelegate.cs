a0b64196da7be19fd330279e0aa2dfed146b433e
gtomkiel <grzegorz.tomkiel@student.nhlstenden.com>
2025-04-01 10:44:18 +0200

    Fix template

---
+++ crypto/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace crypto;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
